6343f021283cfbae8e85c6bcbf65fcd1a09f502b
notes <notes@localhost.localdomain>
2012-02-17 01:37:13 +0900

    Unicode basic operation

---
+++ python/unicode.py
@@ -0,0 +1,13 @@
+# code: utf-8
+
+# Unicode string
+u"テスト"
+# literal of UCS2 code point
+u"\u1234"
+# literal of UCS4 code point
+u"\U00012345"
+# code point to Unicode string
+unichr(100)
+# Unicode string to code point
+ord(u"あ")
+
