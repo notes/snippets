54c582e52d04a19f7e9461a94e59d1b980e0a4d4
notes <notes@localhost.localdomain>
2012-12-05 00:04:36 +0900

    add samples for look-ahead and look-behind assertion

--- perl/re_advanced.pl
+++ perl/re_advanced.pl
@@ -1,9 +1,31 @@
+# positive look-ahead assertion
+my $mailaddr1 = 'bob@example.jp';
+$mailaddr1 =~ s/bob(?=\@example\.jp)/bill/;
+print "$mailaddr1\n";
+
+# negative look-ahead assertion
+my $mailaddr2 = 'alice@example.net';
+$mailaddr2 =~ s/alice(?!\@example\.jp)/sue/;
+print "$mailaddr2\n";
+
+# positive look-behind assertion
+my $mailaddr3 = 'michael@example.org';
+$mailaddr3 =~ s/michael@\Kexample\.org/example.com/;
+print "$mailaddr3\n";
+
+# negative look-behind assertion
+my $mailaddr4 = 'peter@example.info';
+$mailaddr4 =~ s/(?<!dave@)example\.info/example.jp/;
+print "$mailaddr4\n";
+
 # capture quoted-string with escape
 my $quoted_string = '"foo \"bar\" baz"';
+
 # using possessive quantifier
-# prevent backtracking
+# to prevent backtracking
 $quoted_string =~ /"(?:[^"\\]++|\\.)*+"/;
 print "$&\n";
+
 # using independent grouping
 # the same meaning, but hard to read
 $quoted_string =~ /"(?>(?:(?>[^"\\]+)|\\.)*)"/;
