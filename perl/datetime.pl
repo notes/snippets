c7d001438102072a99dd65a2412dd593477bf928
notes <notes@localhost.localdomain>
2013-01-04 22:10:51 +0900

    change function name: iso8601 => rfc3339. add parse_generalized_time().

--- perl/datetime.pl
+++ perl/datetime.pl
@@ -10,10 +10,12 @@ our @DAY_OF_WEEK = qw(Sun Mon Tue Wed Thu Fri Sat);
 our @MONTH_NAME = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
 
 print format_822date(time), "\n";
-print format_iso8601(time), "\n";
+print format_3339date(time), "\n";
 print format_generalized_time(time), "\n";
 print format_asctime(time), "\n";
 print parse_822date(format_822date(time)), "\n";
+print parse_3339date(format_3339date(time)), "\n";
+print parse_generalized_time(format_generalized_time(time)), "\n";
 
 sub format_822date {
     my $t = Time::localtime::localtime(shift);
@@ -51,28 +53,77 @@ sub parse_822date {
     }
 }
 
-sub format_iso8601 {
+# RFC 3339 contains a profile of ISO 8601 for Internet use.
+sub format_3339date {
     my $t = Time::localtime::localtime(shift);
     return sprintf("%04d-%02d-%02dT%02d:%02d:%02d%+03d:00",
-        $t->year + 1900, $t->mon, $t->mday, $t->hour, $t->min, $t->sec,
+        $t->year + 1900, $t->mon + 1, $t->mday, $t->hour, $t->min, $t->sec,
         timezone_offset() / 3600);
 }
 
+sub parse_3339date {
+    my $time = shift;
+    my $pattern = qr/
+        \A
+        (?P<year>\d{4})-(?P<month>\d{2})-(?P<mday>\d{2})T  # year-month-mday
+        (?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})      # hour-min-sec
+        (?:\.(?P<fraction>\d+))?                           # fraction
+        (?P<tzoffset>Z|[+-]\d{2}:\d{2})?                   # timezone offset
+        \z
+    /x;
+    return undef unless $time =~ $pattern;
+
+    my ($mday, $month, $year, $hour, $min, $sec, $tzoffset) =
+        @+{qw(mday month year hour min sec tzoffset)};
+    if (defined $tzoffset) {
+        my $gmt = timegm($sec, $min, $hour, $mday, $month - 1, $year);
+        if ($tzoffset eq 'Z') {
+            return $gmt;
+        }
+        return $gmt - substr($tzoffset, 0, 3) * 3600;
+    } else {
+        return timelocal($sec, $min, $hour, $mday, $month - 1, $year);
+    }
+}
+
+# RFC 4517 contains the definition of LDAP Generalized Time syntax.
 sub format_generalized_time {
     my $t = Time::localtime::localtime(shift);
     return sprintf("%04d%02d%02d%02d%02d%02d%+03d00",
-        $t->year + 1900, $t->mon, $t->mday, $t->hour, $t->min, $t->sec,
+        $t->year + 1900, $t->mon + 1, $t->mday, $t->hour, $t->min, $t->sec,
         timezone_offset() / 3600);
 }
 
+sub parse_generalized_time {
+    my $time = shift;
+    my $pattern = qr/
+        \A
+        (?P<year>\d{4})(?P<month>\d{2})(?P<mday>\d{2})  # year-month-mday
+        (?P<hour>\d{2})(?P<min>\d{2})(?P<sec>\d{2})     # hour-min-sec
+        (?:[.:](?P<fraction>\d+))?                      # fraction
+        (?P<tzoffset>Z|[+-]\d{4})?                      # timezone offset
+        \z
+    /x;
+    return undef unless $time =~ $pattern;
+
+    my ($mday, $month, $year, $hour, $min, $sec, $tzoffset) =
+        @+{qw(mday month year hour min sec tzoffset)};
+    if (defined $tzoffset) {
+        my $gmt = timegm($sec, $min, $hour, $mday, $month - 1, $year);
+        if ($tzoffset eq 'Z') {
+            return $gmt;
+        }
+        return $gmt - substr($tzoffset, 0, 3) * 3600;
+    } else {
+        return timelocal($sec, $min, $hour, $mday, $month - 1, $year);
+    }
+}
+
 sub format_asctime {
-    use POSIX qw(tzname);
     my $t = Time::localtime::localtime(shift);
-    my @tznames = tzname();
-    return sprintf("%s %s %2d %02d:%02d:%02d %s %04d",
+    return sprintf("%s %s %2d %02d:%02d:%02d %04d",
         $DAY_OF_WEEK[$t->wday], $MONTH_NAME[$t->mon], $t->mday,
-        $t->hour, $t->min, $t->sec,
-        $t->isdst ? $tznames[1] : $tznames[0], $t->year + 1900);
+        $t->hour, $t->min, $t->sec, $t->year + 1900);
 }
 
 sub timezone_offset {
