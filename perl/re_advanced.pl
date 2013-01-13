ed7c31e7ebcf93ee096327956ff23828eb1506e9
notes <notes@localhost.localdomain>
2013-01-13 23:53:37 +0900

    make variable interpolation explicit

--- perl/re_advanced.pl
+++ perl/re_advanced.pl
@@ -1,22 +1,22 @@
 # positive look-ahead assertion
 my $mailaddr1 = 'bob@example.jp';
 $mailaddr1 =~ s/bob(?=\@example\.jp)/bill/;
-print "$mailaddr1\n";
+print "${mailaddr1}\n";
 
 # negative look-ahead assertion
 my $mailaddr2 = 'alice@example.net';
 $mailaddr2 =~ s/alice(?!\@example\.jp)/sue/;
-print "$mailaddr2\n";
+print "${mailaddr2}\n";
 
 # positive look-behind assertion
 my $mailaddr3 = 'michael@example.org';
 $mailaddr3 =~ s/michael@\Kexample\.org/example.com/;
-print "$mailaddr3\n";
+print "${mailaddr3}\n";
 
 # negative look-behind assertion
 my $mailaddr4 = 'peter@example.info';
 $mailaddr4 =~ s/(?<!dave@)example\.info/example.jp/;
-print "$mailaddr4\n";
+print "${mailaddr4}\n";
 
 # capture quoted-string with escape
 my $quoted_string = '"foo \"bar\" baz"';
