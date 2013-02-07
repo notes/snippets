ed7895063c1c0a8d58f581810b3dfd77a09169c0
notes <notes@localhost.localdomain>
2013-02-08 00:40:08 +0900

    add JIT compiler related options

--- java/launch.sh
+++ java/launch.sh
@@ -16,12 +16,17 @@ java \
  -Xms2G \
  -Xmx2G \
  -Xloggc:/var/tmp/gc.log \
+ -Xint \
  -XX:NewSize=1G \
  -XX:MaxNewSize=1G \
  -XX:PermSize=512M \
  -XX:MaxPermSize=512M \
  -XX:+PrintCommandLineFlags \
  -XX:+PrintCompilation \
+ -XX:CompileCommand=exclude,java.util.HashMap,* \
+ -XX:+CITime \
+ -XX:+PrintInlining \
+ -XX:MaxInlineSize=100 \
  -XX:+HeapDumpOnOutOfMemoryError \
  -XX:+DisableExplicitGC \
  -XX:+PrintGCDetails \
@@ -31,3 +36,5 @@ java \
  -XX:+PrintGCApplicationConcurrentTime \
  -XX:+TraceClassLoading \
  -XX:+TraceClassUnloading \
+ -jar foo.jar
+
