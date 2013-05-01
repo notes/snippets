6e2c63b7574507dd927050ed46905f954c7b074c
notes <notes@localhost.localdomain>
2013-05-01 18:02:27 +0900

    add commands on elastic IP address

--- aws/awscli.sh
+++ aws/awscli.sh
@@ -13,6 +13,12 @@ aws ec2 create-key-pair --key-name my-key-pair
 # check created key-pairs
 aws ec2 describe-key-pairs
 
+# allocate new IP address
+aws ec2 allocate-address
+
+# check allocated IP address
+aws ec2 describe-addresses
+
 # search AMI images
 aws ec2 describe-images --owners amazon --filters '[
   { "name":"root-device-type",    "values":"instance-store" },
@@ -27,8 +33,17 @@ aws ec2 run-instances --image-id ami-173fbf16 --min-count 1 --max-count 1 \
 # check running instances
 aws ec2 describe-instances --instance-ids i-12345678
 
+# or if you want status only
+aws ec2 describe-instance-status --include-all-instances
+
+# associate public IP address with instance
+aws ec2 associate-address --instance-id i-12345678 --public-ip 192.168.0.1
+
 # stop & save EBS instances
 aws ec2 stop-instances --instance-ids i-12345678
+
+# restart saved EBS instances
+aws ec2 start-instances --instance-ids i-12345678
 
 # terminate instances (this destroys all instance data)
 aws ec2 terminate-instances --instance-ids i-12345678
